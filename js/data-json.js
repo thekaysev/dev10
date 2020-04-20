b7e4769452ad923650c3e9f2ea34f2fc987652a0
thekaysev <48273558+thekaysev@users.noreply.github.com>
2020-04-20 13:43:19 -0500

    Update data-json.js

--- js/data-json.js
+++ js/data-json.js
@@ -14,7 +14,7 @@ xhr.onload = function() {                       // When readystate changes
     for (var i = 0; i < responseObject.events.length; i++) { // Loop through object
       newContent += '<div class="event">';
       newContent += '<img src="' + responseObject.events[i].map + '" ';
-      newContent += 'height = "310" width = "310" '; + responseObject.events[i].location + '" />';
+      newContent += 'height = "350" width = "350" '; + responseObject.events[i].location + '" />';
       newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
       newContent += responseObject.events[i].date + '</p>';
       newContent += '</div>';
